ff2625fe63920fe7e14aff21c1bd77cdb6696a48
Mike Jones, II <micujonesii@gmail.com>
2025-02-22 13:45:18 -0500

    Use existing token

--- auth.js
+++ auth.js
@@ -9,28 +9,49 @@ let generateJWTToken = (user) => {
     return jwt.sign(user, jwtSecret, {
         subject: user.username,
         expiresIn: '7d',
-        algorithm: 'HS256'
+        algorithm: 'HS256',
     });
-}
+};
 
 module.exports = (router) => {
     router.post('/login', (req, res) => {
-        passport.authenticate('local', { session: false },
+        passport.authenticate(
+            'local',
+            { session: false },
             (error, user, info) => {
                 if (error || !user) {
                     return res.status(400).json({
                         message: 'Something is not right.',
-                        user: user
+                        user: user,
                     });
                 }
                 req.login(user, { session: false }, (error) => {
                     if (error) {
                         res.send(error);
                     }
+
+                    // Check for existing token
+                    const existingToken =
+                        req.headers.authorization?.split(' ')[1];
+                    if (existingToken) {
+                        try {
+                            const decoded = jwt.verify(
+                                existingToken,
+                                jwtSecret
+                            );
+                            if (decoded && decoded.exp * 1000 > Date.now()) {
+                                // Token is still valid
+                                return res.json({ user, token: existingToken });
+                            }
+                        } catch (error) {
+                            // Token is invalid or expired
+                        }
+                    }
+
                     let token = generateJWTToken(user.toJSON());
                     return res.json({ user, token });
                 });
             }
-        ) (req, res);
+        )(req, res);
     });
-}
+};
